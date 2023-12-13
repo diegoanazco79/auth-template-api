f1c7ccc0709844717c5acf8180f09fa3b57a9cb7
Diego Añazco <danazco@vozy.co>
2023-12-12 20:39:08 -0500

    :bug: Depuring mongoDB connection

--- dist/index.js
+++ dist/index.js
@@ -7,12 +7,12 @@ require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const config_1 = require("./config/config");
-const routes_1 = __importDefault(require("./routes"));
+const mongo_1 = __importDefault(require("./config/mongo"));
 const PORT = config_1.config.port || 3001;
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use(routes_1.default);
-// dbConnect().then(() => console.log("MongoDB connected"));
+// app.use(router);
+(0, mongo_1.default)().then(() => console.log("MongoDB connected"));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 //# sourceMappingURL=index.js.map
