55138120373e1137affc043cc204a95e78978ea3
Diego Añazco <danazco@vozy.co>
2023-12-12 20:48:37 -0500

    :bug: Depuring :: Adding a post role with express-validator

--- dist/index.js
+++ dist/index.js
@@ -9,12 +9,20 @@ const cors_1 = __importDefault(require("cors"));
 const config_1 = require("./config/config");
 const mongo_1 = __importDefault(require("./config/mongo"));
 const role_1 = require("./controllers/role");
+const express_validator_1 = require("express-validator");
+const role_2 = require("./db-validators/role");
+const errorHandlers_1 = require("./utils/errorHandlers");
 const PORT = config_1.config.port || 3001;
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 // app.use(router);
 app.get("/role", role_1.getAllRoles);
+app.post("/", [
+    (0, express_validator_1.check)("name", "Name is required").not().isEmpty(),
+    (0, express_validator_1.check)("name", "Name must be unique").custom(role_2.uniqueRoleName),
+    errorHandlers_1.validateFields,
+], role_1.postRole);
 (0, mongo_1.default)().then(() => console.log("MongoDB connected"));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ import cors from "cors";
 import { config } from "./config/config";
 import dbConnect from "./config/mongo";
 import router from "./routes";
-import { getAllRoles } from "./controllers/role";
+import { getAllRoles, postRole } from "./controllers/role";
+import { check } from "express-validator";
+import { uniqueRoleName } from "./db-validators/role";
+import { validateFields } from "./utils/errorHandlers";
 
 const PORT = config.port || 3001;
 
@@ -15,6 +18,15 @@ app.use(express.json());
 // app.use(router);
 
 app.get("/role", getAllRoles)
+app.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("name", "Name must be unique").custom(uniqueRoleName),
+    validateFields,
+  ],
+  postRole
+);
 
 dbConnect().then(() => console.log("MongoDB connected"));
 
