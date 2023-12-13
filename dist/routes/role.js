f96c9488dc8ef40ba5bc737b69925c2bc305df5f
Diego Añazco <danazco@vozy.co>
2023-12-12 20:57:16 -0500

    :bug: Depuring: Adding a roleRouter in index

--- dist/routes/role.js
+++ dist/routes/role.js
@@ -2,25 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
-// import { check } from "express-validator";
+const express_validator_1 = require("express-validator");
 const role_1 = require("../controllers/role");
+const role_2 = require("../db-validators/role");
 const errorHandlers_1 = require("../utils/errorHandlers");
 const router = (0, express_1.Router)();
 exports.router = router;
 router.get("/", role_1.getAllRoles);
 router.post("/", [
-    // check("name", "Name is required").not().isEmpty(),
-    // check("name", "Name must be unique").custom(uniqueRoleName),
+    (0, express_validator_1.check)("name", "Name is required").not().isEmpty(),
+    (0, express_validator_1.check)("name", "Name must be unique").custom(role_2.uniqueRoleName),
     errorHandlers_1.validateFields,
 ], role_1.postRole);
 router.put("/:id", [
-    // check("id").custom(existRoleId),
-    // check("name", "Name is required").not().isEmpty(),
-    // check("name", "Name must be unique").custom(uniqueRoleName),
+    (0, express_validator_1.check)("id").custom(role_2.existRoleId),
+    (0, express_validator_1.check)("name", "Name is required").not().isEmpty(),
+    (0, express_validator_1.check)("name", "Name must be unique").custom(role_2.uniqueRoleName),
     errorHandlers_1.validateFields,
 ], role_1.putRole);
-router.delete("/:id", [
-    // check("id").custom(existRoleId), 
-    errorHandlers_1.validateFields
-], role_1.deleteRole);
+router.delete("/:id", [(0, express_validator_1.check)("id").custom(role_2.existRoleId), errorHandlers_1.validateFields], role_1.deleteRole);
 //# sourceMappingURL=role.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,14 @@ import cors from "cors";
 
 import { config } from "./config/config";
 import dbConnect from "./config/mongo";
-import router from "./routes";
-import { getAllRoles, postRole } from "./controllers/role";
-import { check } from "express-validator";
-import { uniqueRoleName } from "./db-validators/role";
-import { validateFields } from "./utils/errorHandlers";
+import { router as roleRouter } from "./routes/role";
 
 const PORT = config.port || 3001;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-// app.use(router);
-
-app.get("/role", getAllRoles)
-app.post(
-  "/",
-  [
-    check("name", "Name is required").not().isEmpty(),
-    check("name", "Name must be unique").custom(uniqueRoleName),
-    validateFields,
-  ],
-  postRole
-);
+app.use(roleRouter);
 
 dbConnect().then(() => console.log("MongoDB connected"));
 
